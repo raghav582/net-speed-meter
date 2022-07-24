c04e72d484d0be4d436a1c8a5abff51503debc44
raghav582 <raghavsharma582@gmail.com>
2022-07-24 21:56:53 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'net-speed-meter',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
