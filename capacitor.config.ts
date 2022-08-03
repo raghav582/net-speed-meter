a1c5ed658ce6ebb40c3f7d16960deb87683adc44
raghav582 <raghavsharma582@gmail.com>
2022-08-03 21:06:37 +0530

    changed name and icon

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.net.speed.meter',
-  appName: 'net-speed-meter',
+  appName: 'Net Speed Meter',
   webDir: 'www',
   bundledWebRuntime: false
 };
